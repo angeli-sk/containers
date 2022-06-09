1785351e80a8b0fa5c1c74cb044ed74e685e8007
angeli-sk <akramp@f1r4s3.codam.nl>
2022-06-09 19:19:03 +0200

    Didnt change much

--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: akramp <akramp@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/26 11:59:32 by akramp        #+#    #+#                 */
-/*   Updated: 2022/06/07 17:26:18 by akramp        ########   odam.nl         */
+/*   Updated: 2022/06/09 19:18:23 by akramp        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,33 +24,14 @@ namespace ft
 			typedef std::allocator_traits<allocator_type>   alloc_traits;
 			typedef typename alloc_traits::size_type       	size_type;
 
-			explicit vector (const allocator_type& alloc = allocator_type()) : _alloc(alloc) {}
-			// explicit vector (size_type n, const value_type& val = value_type(),
-			// 	const allocator_type& alloc = allocator_type());
-			// vector (InputIterator first, InputIterator last,
-            //     const allocator_type& alloc = allocator_type());
-			vector(const vector  & copy) { *this = copy; }
-			vector & operator = (vector const & copy)
-			{
-				this->_length = copy._length;
-				return *this;
-			}
-			virtual ~vector() {};
-
 		private:
+			// __storage_pointer                                      _begin;
+			// size_type                                              _size;
 			T			_length;
-			Allocator 	_alloc;
+			Allocator 	_alloc_type;
+			Allocator	*_alloc;
 
 		protected:
-			// template<typename T_>
-			// struct is_same<T_,T_>
-			// {
-			// 	static const bool value = true;
-			// };
-
-			// template<bool> struct static_assert;
-			// template<> struct static_assert<true> {};
-
 			typedef T												value_type;
 			typedef value_type&										reference;
 			typedef const value_type&								const_reference;
@@ -62,7 +43,33 @@ namespace ft
 			typedef std::reverse_iterator<iterator>          		reverse_iterator;
 			typedef std::reverse_iterator<const_iterator>    		const_reverse_iterator;
 
+		public:
+			explicit vector (const allocator_type& alloc = allocator_type()) : _alloc_type(alloc) {}
 
+			explicit vector (size_type n, const value_type& val = value_type(),
+				const allocator_type& alloc = allocator_type());
+			// {
+					//asign
+			// }
+
+			template <class InputIterator>
+				vector (InputIterator first, InputIterator last,
+					const allocator_type& alloc = allocator_type());
+			// {
+
+			// }
+
+			vector(const vector  & copy) { *this = copy; }
+			vector & operator = (vector const & copy)
+			{
+				this->_length = copy._length;
+				return *this;
+			}
+			virtual ~vector()
+			{
+				if (_alloc)
+					delete _alloc;
+			};
 			// static_assert<is_same<typename allocator_type::value_type,value_type>::value>();
 			//begin();
 	};
